63d204aca239226197d0b5589adefc3ed7b073b5
Eduardo Cruz <eduardo_acj2@hotmail.com>
2017-09-20 17:57:04 -0300

    implementacao da classe amostra

---
+++ src/Entidades/Amostra.java
@@ -0,0 +1,70 @@
+
+package Entidades;
+
+public class Amostra {
+    
+    private int id;
+    private Double carboidratos;
+    private Double cinzas;
+    private Double fibras;
+    private Double lipidios;
+    private Double proteinas;
+    private Double umidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getCarboidratos() {
+        return carboidratos;
+    }
+
+    public void setCarboidratos(Double carboidratos) {
+        this.carboidratos = carboidratos;
+    }
+
+    public Double getCinzas() {
+        return cinzas;
+    }
+
+    public void setCinzas(Double cinzas) {
+        this.cinzas = cinzas;
+    }
+
+    public Double getFibras() {
+        return fibras;
+    }
+
+    public void setFibras(Double fibras) {
+        this.fibras = fibras;
+    }
+
+    public Double getLipidios() {
+        return lipidios;
+    }
+
+    public void setLipidios(Double lipidios) {
+        this.lipidios = lipidios;
+    }
+
+    public Double getProteinas() {
+        return proteinas;
+    }
+
+    public void setProteinas(Double proteinas) {
+        this.proteinas = proteinas;
+    }
+
+    public Double getUmidade() {
+        return umidade;
+    }
+
+    public void setUmidade(Double umidade) {
+        this.umidade = umidade;
+    }
+    
+}
